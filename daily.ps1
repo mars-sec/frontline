949524fb8dcda9a8f273a90f4a9db6978cbb2479
mars-sec <marshall.e.yanis@gmail.com>
2026-08-31 22:05:49 -0400

    Personal version: Vercel deployment, edition footer links, daily script

---
+++ daily.ps1
@@ -0,0 +1,16 @@
+$ErrorActionPreference = "Stop"
+Set-Location $PSScriptRoot
+
+frontline run
+if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
+
+$today = Get-Date -Format "yyyy-MM-dd"
+$edition = "editions/$today.html"
+
+if (Test-Path $edition) {
+    git add editions/
+    git commit -m "edition $today"
+    git push personal main
+} else {
+    Write-Host "No new edition generated."
+}
